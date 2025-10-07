813a3f8e8aea5fcd2afcac08c73056cac67e3a46
Delbert <delbertmelvin1209@gmail.com>
2025-10-07 22:12:11 +0700

    Initial commit - Xcode project

---
+++ SITARJO/SITARJOApp.swift
@@ -0,0 +1,17 @@
+//
+//  SITARJOApp.swift
+//  SITARJO
+//
+//  Created by prk on 07/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SITARJOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ControlPage()
+        }
+    }
+}
